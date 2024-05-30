37ea723d816306e02ec0c70dd905b7c8b014c297
angel <angel@angel@acidburn.tech>
2024-05-30 13:02:22 -0700

    helm

--- init.el
+++ init.el
@@ -17,68 +17,67 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
+
 ;; SETTINGS
-(setq confirm-kill-emacs 'yes-or-no-p ;; don't accidentally leave
-      evil-want-C-u-scroll 1 ;; old fashioned evil
-      ivy-use-virtual-buffers t ;; i dont know ivy shit ugh :FIXME:
-      evil-want-keybinding nil ;; evil wants
-      evil-want-integration t ;; what evil wants
-      tramp-default-method "ssh" ;; faster tramp with ssh
-      visible-bell 1) ;; don't make noise
-(set-frame-font "Fira Code 26") ;; granny-coded
-(add-hook 'prog-mode-hook 'line-number-mode) ;; lines
+(setq
+ confirm-kill-emacs 'yes-or-no-p 
+ enable-recursive-minibuffers t
+ evil-want-C-u-scroll 1 ;; old fashioned evil (repent)
+ evil-want-integration t ;; evil-collection config (repent)
+ evil-want-keybinding nil ;; evil-collection config (repent)
+ org-refile-targets '(("gtd.org" :maxlevel . 2)
+		      ("someday.org" :maxlevel . 2))
+ tramp-default-method "ssh" ;; faster tramp with ssh
+ ;; set backup and autosave directory
+ auto-save-default nil
+ make-backup-files nil
+ backup-directory-alist '(("." . "/tmp")) 
+ visible-bell 1) ;; don't make noise
+
 (add-hook 'prog-mode-hook 'column-number-mode) ;; columns
-(fset 'yes-or-no-p 'y-or-n-p) ;; i'm lazy
-(tool-bar-mode -1)     ;; no gui
-(menu-bar-mode -1)     ;; no nothin
-(toggle-scroll-bar -1) ;; no how
+(add-hook 'prog-mode-hook 'line-number-mode) ;; lines
 (electric-pair-mode 1) ;; birds of a feather
+(fset 'yes-or-no-p 'y-or-n-p) ;; i'm lazy
+(menu-bar-mode -1) ;; no gui
 (recentf-mode 1) ;; what was i doing?
+(set-frame-font "Fira Code 22") ;; granny-coded
 (show-paren-mode 1) ;; i believe in symmetry 
+(toggle-scroll-bar -1) ;; no gui
+(tool-bar-mode -1) ;; no gui
 
 ;; WM
-(straight-use-package 'exwm) 
-(require 'exwm-config)
-(require 'exwm-randr)
-(exwm-randr-enable)
-(exwm-config-ido)
-(exwm-init)
+;; (straight-use-package 'exwm) 
+;; (require 'exwm-config)
+;; (require 'exwm-randr)
+;; (exwm-randr-enable)
+;; (exwm-config-ido)
+;; (exwm-init)
 
 ;; UTIL
-(straight-use-package 'zen-mode)
-(straight-use-package 'which-key)
+(straight-use-package 'which-key) ;; keep forgetting my binds
 (which-key-mode)
-(straight-use-package 'vterm)
+(straight-use-package 'rainbow-delimiters)
+(rainbow-delimiters-mode 1)
+(straight-use-package 'vterm) ;; erm? idk
 (straight-use-package 'doom-themes)
 (straight-use-package 'nerd-icons)
 (load-theme 'doom-badger t)
 (straight-use-package 'doom-modeline)
 (doom-modeline-mode)
-(straight-use-package 'iedit)
-
-;; COMPLETION
-(straight-use-package 'company)
-(global-company-mode 1)
+(straight-use-package 'iedit) ;; yagni?
 
 ;; NAVIGATION
-(straight-use-package 'projectile)
-(straight-use-package 'ivy)
-(straight-use-package 'counsel)
-(ivy-mode 1)
-(setq ivy-re-builders-alist
-      '((t . ivy--regex-fuzzy)))
-(counsel-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-(setq search-default-mode #'char-fold-to-regexp)
+(straight-use-package 'helm)
+(helm-mode)
 
-(projectile-mode 1)
+;; COMPLETION
+(straight-use-package 'company) ;; replace with complete?
+(global-company-mode 1)
 
-;; EVIL
+;; EVIL (until i repent)
 (straight-use-package 'evil)
 (straight-use-package 'evil-surround)
 (straight-use-package 'evil-commentary)
-(straight-use-package 'evil-snipe)
 (straight-use-package 'evil-iedit-state)
 (straight-use-package 'evil-collection)
 (evil-mode 1)
@@ -88,23 +87,25 @@
 (evil-global-set-key 'normal "H" 'tab-next)
 (evil-global-set-key 'normal "L" 'tab-previous)
 
-;; LEADER
+;; LEADER (false idol)
 (straight-use-package 'evil-leader)
 (global-evil-leader-mode)
 (evil-leader/set-leader "<SPC>")
 (evil-leader/set-key
-  "/" 'swiper-isearch
-  "a" 'vterm
-  "b" 'ivy-switch-buffer
-  "e" 'counsel-find-file
-  "f" 'counsel-dired
-  "hf" 'counsel-describe-function
-  "hi" 'counsel-info-lookup-symbol
-  "hl" 'counsel-find-library
-  "hv" 'counsel-describe-variable
+  "/" 'isearch
+  "a" 'eshell
+  "b" 'display-buffer
+  "e" 'find-file
+  "f" 'dired
+  "ha" 'apropos
+  "hf" 'describe-function
+  "hi" 'info-lookup-symbol
+  "hl" 'find-library
+  "hv" 'describe-variable
   "k" 'kill-buffer-and-window
-  "m" 'counsel-recentf
-  "p" 'counsel-yank-pop
+  "m" 'recentf
+  ;; "o" 'unused
+  ;; "p" 'unused
   "oa" 'org-agenda
   "oc" 'org-capture
   "sh" 'split-window-horizontally
@@ -114,13 +115,19 @@
   "t" 'tab-new
   "u" 'eww
   "w" 'save-buffer
-  "x" 'counsel-M-x)
+  "x" 'helm-M-x)
 
 ;; ORG
 (straight-use-package 'evil-org)
 (add-hook 'org-mode-hook 'evil-org-mode)
-(setq org-refile-targets '(("gtd.org" :maxlevel . 2)
-			   ("someday.org" :maxlevel . 2)))
+
 ;; LANG
-(straight-use-package 'rainbow-delimiters)
-(rainbow-delimiters-mode 1)
+(straight-use-package 'handlebars-mode)
+
+
+;; LSP
+(with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs
+	       '(handlebars-mode . ("node",
+				    "/home/angel/.emacs.d/ember-language-server/lib/start-server.js"
+				    "--stdio"))))
