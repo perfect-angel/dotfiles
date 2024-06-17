c84803ad2ead6b739c4fc321eee7479151759ad6
Angel Campbell <angel@acidburn.tech>
2024-06-17 06:55:36 -0700

    hyperrr

--- init.el
+++ init.el
@@ -23,10 +23,11 @@
 ;; SETTINGS
 (when (eq system-type 'darwin) ;; mac specific settings
   (setq mac-option-modifier 'meta)
-  (setq mac-command-modifier 'meta)
+  (setq mac-command-modifier 'alt)
   (global-set-key [kp-delete] 'delete-char) ;; sets fn-delete to be right-delete
   )
 
+(setq-default fill-column 80)
 (setq
  browse-url-browser-function 'eww-browse-url
  confirm-kill-emacs 'yes-or-no-p 
@@ -49,20 +50,25 @@
  make-backup-files nil ;; no backup files 
  backup-directory-alist '(("." . "/tmp")) ;; send backups to hell
  ring-bell-function 'ignore
+ rcirc-server-alist
+      '(("irc.libera.chat" ; Hostname
+       :port 6697          ; Standard port for secure IRC
+       :nick "perfect-angel"       ; Your nickname
+       :encryption tls     ; Specify to use encryption
+       :channels ("#emacs")))
  which-key-show-early-on-C-h t
  which-key-idle-delay 0.3
  visible-bell 1) ;; don't make noise
 
 (add-hook 'prog-mode-hook 'column-number-mode) ;; columns
 (add-hook 'prog-mode-hook 'line-number-mode) ;; lines
-(add-hook 'prog-mode-hook #'auto-fill-mode) ;; word wrap
-(setq-default fill-column 80)
+(add-hook 'prog-mode-hook 'auto-fill-mode) ;; word wrap
 (electric-pair-mode 1) ;; birds of a feather
 (electric-indent-mode 1)
 (fset 'yes-or-no-p 'y-or-n-p) ;; i'm lazy
 (menu-bar-mode -1) ;; no gui
 (recentf-mode 1) ;; what was i doing?
-(set-frame-font "Inconsolata Nerd Font Mono 22") ;; granny-coded
+(set-frame-font "Inconsolata Nerd Font Mono 25") ;; granny-coded
 (show-paren-mode 1) ;; i believe in symmetry 
 (toggle-scroll-bar -1) ;; no gui
 (tool-bar-mode -1) ;; no gui
@@ -76,7 +82,6 @@
 ;; (exwm-init)
 
 ;; UTIL
-(straight-use-package 'which-key) ;; keep forgetting my binds
 (straight-use-package 'magit) ;; magic git
 (straight-use-package 'vterm) ;; erm? idk
 (straight-use-package 'doom-themes)
@@ -90,50 +95,57 @@
 (straight-use-package 'discover) ;; learn betterer
 (straight-use-package 'lingva) ;; translate
 
+(global-discover-mode 1)
 (exec-path-from-shell-initialize)
 (doom-modeline-mode)
 (rainbow-delimiters-mode 1)
-(which-key-mode)
-(rainbow-delimiters-mode 1)
-(which-key-mode)
 
 (load-theme 'doom-miramare t)
 
-;; NAVIGATION
+;; NAVIGATION 🗺️
 (straight-use-package 'helm)
 (helm-mode 1)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 
-;; INTERNET
+;; INTERNET 🌐
 (straight-use-package 'mastodon)
 
 (require 'mastodon-discover)
 (with-eval-after-load 'mastodon (mastodon-discover))
 
-;; COMPLETION
+;; COMPLETION 🧩
 (straight-use-package 'company) ;; replace with complete?
 (global-company-mode 1)
 
-;; EVIL (until i repent)
+;; EVIL (until i repent) 😈
 (straight-use-package 'evil)
 
 (global-set-key (kbd "M-<return>") 'evil-mode)
 
-;; ORG
+;; ORG 📆
 (require 'org)
 
 (global-set-key (kbd "C-c o a") #'org-agenda)
 (global-set-key (kbd "C-c o c") #'org-capture)
 
-;; LANG
+;; LANG 📕
 (straight-use-package 'handlebars-mode)
 (straight-use-package 'geiser-guile)
+
+;; OPS 💻
 (straight-use-package 'kubernetes)
 
-;; LSP
+;; LSP 
 (with-eval-after-load 'eglot
   (add-to-list 'eglot-server-programs
       '(handlebars-mode . ("node",
    "/home/angel/.emacs.d/ember-language-server/lib/start-server.js"
    "--stdio"))))
+
+;; HYPER 🤪
+(global-set-key (kbd "A-C-M-a") 'vterm)
+(global-set-key (kbd "A-C-M-/") 'helm-do-grep-ag)
+(global-set-key (kbd "A-C-M-f") 'helm-find-files)
+(global-set-key (kbd "A-C-M-e") 'emoji-insert)
+
