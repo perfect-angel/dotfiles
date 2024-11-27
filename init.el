d7e6391feb53a9e84e521f9fcb9b0f0b852b8924
Angel <angel@acidburn.tech>
2024-11-27 13:29:25 -0800

    nongnu

--- init.el
+++ init.el
@@ -5,6 +5,8 @@
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
 (package-initialize)
 
 
