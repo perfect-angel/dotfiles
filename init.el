5a2fc2f098389101bacc28f8a7a2889ca5a69322
Angel <angel@acidburn.tech>
2024-12-27 12:01:02 -0800

    wip

--- init.el
+++ init.el
@@ -2,20 +2,8 @@
 ;;; Commentary:
 ;;; this is not a good endorsement of me as a serious person
 ;;; Code:
-(require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives
-	     '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
-(package-initialize)
 
-
-(require 'use-package)
-(setq use-package-always-ensure t)
-
-(org-babel-load-file "~/dotfiles/config.org")
+(org-babel-load-file "~/org/config.org")
 
 (provide 'init)
 ;;; init.el ends here
-(put 'narrow-to-region 'disabled nil)
-(put 'narrow-to-page 'disabled nil)
