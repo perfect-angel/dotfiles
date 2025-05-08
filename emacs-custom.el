759822ea08673c385902a52a911dc56163c139e6
Angel <angel@acidburn.tech>
2025-05-08 08:58:55 -0700

    custom

---
+++ emacs-custom.el
@@ -0,0 +1,19 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(all-the-icons breadcrumb dap-mode dired-sidebar doom-modeline elfeed
+		   elisp-demos embark emmet-mode exec-path-from-shell
+		   geiser-guile kubernetes magit marginalia mini-frame orderless
+		   org-bullets rainbow-delimiters rust-mode spray swiper
+		   tree-sitter-indent tree-sitter-langs treesit-auto
+		   visual-fill-column vterm yasnippet-snippets)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init.el
+++ init.el
@@ -1,4 +1,3 @@
 ;; -*- lexical-binding: t; -*-
 (require 'org)
 (org-babel-load-file "~/dotfiles/config.org")
-
