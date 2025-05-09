fa32c8342004cc51553bbfdcf45b3b9005e5fc52
Angel <angel@acidburn.tech>
2025-05-09 08:17:28 -0700

    dashboard

--- emacs-custom.el
+++ emacs-custom.el
@@ -5,12 +5,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(all-the-icons breadcrumb dap-mode dired-sidebar doom-modeline elfeed
-		   elisp-demos embark emmet-mode exec-path-from-shell
-		   geiser-guile kubernetes magit marginalia mini-frame orderless
-		   org-bullets rainbow-delimiters rust-mode spray swiper
-		   tree-sitter-indent tree-sitter-langs treesit-auto
-		   visual-fill-column vterm yasnippet-snippets)))
+   '(all-the-icons breadcrumb consult corfu dap-mode dashboard dired-sidebar
+		   doom-modeline elfeed elisp-demos embark embark-consult
+		   emmet-mode exec-path-from-shell geiser-guile kubernetes magit
+		   marginalia mini-frame orderless org-bullets
+		   rainbow-delimiters rust-mode spray swiper tree-sitter-indent
+		   tree-sitter-langs treesit-auto vertico visual-fill-column
+		   vterm yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
