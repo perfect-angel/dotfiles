7e8a1d39af1b2e94c0fcbf404e58bc7c3ee2f8f4
Angel <angel@acidburn.tech>
2025-05-14 04:23:34 -0700

    stuffs

--- emacs-custom.el
+++ emacs-custom.el
@@ -4,13 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(all-the-icons breadcrumb cape corfu dap-mode dashboard devil dired-sidebar
-		   doom-modeline elfeed elisp-demos embark-consult emmet-mode
-		   exec-path-from-shell geiser-guile kubernetes magit marginalia
-		   mini-frame orderless rainbow-delimiters rust-mode
-		   simple-httpd spray tree-sitter treesit-auto vertico
-		   visual-fill-column vterm web-server yasnippet-snippets)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
